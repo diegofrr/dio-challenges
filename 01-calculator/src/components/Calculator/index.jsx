c0b145e46ed56cb391384b2a199a2291ac21c83b
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-30 12:15:59 -0300

    feat: update calculator

--- 01-calculator/src/components/Calculator/index.jsx
+++ 01-calculator/src/components/Calculator/index.jsx
@@ -4,26 +4,34 @@ import { Button } from '../Button';
 import { Display } from '../Display';
 import { useState } from 'react';
 
-const OPERATIONS = ['+', '-', '/', 'x']
+const OPERATIONS = ['+', '-', '/', 'x'];
+const MAX_LABELS = 12;
 
 export const Calculator = () => {
     const [result, setResult] = useState(0);
     const [submitted, setSubmitted] = useState(false);
+    const [canSubmit, setCanSubmit] = useState(false);
 
     const addLabel = label => {
-        if (!result.length && (isOperation(label) || isComma(label))) return;
-        else if (isOperation(label) && lastIsOperation()) {
-            return setResult(result.slice(0, -1) + label)
-        }
+        if (!isValid(label)) return setCanSubmit(false);
+        else setCanSubmit(true);
 
-        if (submitted) clear(label);
-        else setResult(result === 0 ? label : result + label);
+        setResult((result === 0 && !isOperation(label)) ? label : result + label);
     }
 
     const handleSubmit = () => {
-        // eslint-disable-next-line no-eval
-        setResult(eval(formatResult(result)));
+        if (!hasOperation() || lastIsOperation()) return;
+
+        executeEval();
+        setCanSubmit(false);
         setSubmitted(true);
+    }
+
+    const executeEval = () => {
+        // eslint-disable-next-line no-eval
+        let _result = Number(eval(formatResult(result)));
+        _result = Number.isInteger(_result) ? _result : _result.toPrecision(6)
+        setResult(_result);
     }
 
     const clear = (initValue) => {
@@ -42,15 +50,37 @@ export const Calculator = () => {
     }
 
     const lastIsOperation = () => {
-        return isOperation(result[result.length - 1]);
+        return isOperation(lastLabel());
+    }
+
+    const lastLabel = () => {
+        return result[result.length - 1];
     }
 
     const formatResult = result => {
-        return result.toString().replace('x', '*').replace('%', '*')
+        return result.toString().replace('x', '*').replace('%', '*');
     }
 
     const isComma = label => {
         return label === '.';
+    }
+
+    const hasOperation = () => {
+        return OPERATIONS.some(op => result.toString().includes(op)) ||
+            lastIsOperation() || isComma(lastLabel());
+    }
+
+    const isValid = label => {
+        if (result.length + 1 >= MAX_LABELS) {
+            return false
+        } else if (!submitted && !result.length && (isOperation(label) || isComma(label))) {
+            return false;
+        } else if ((isComma(label) || isOperation(label)) && isComma(lastLabel())) {
+            return false;
+        } else if (isOperation(label) && lastIsOperation()) {
+            setResult(result.slice(0, -1) + label);
+            return false;
+        } else return true;
     }
 
     return (
@@ -62,8 +92,8 @@ export const Calculator = () => {
                     <div className={styles['header-buttons']}>
                         <Button hl onclick={addLabel} label='x' />
                         <Button hl onclick={addLabel} label='/' />
-                        <Button hl onclick={clear} label='C' />
-                        <Button hl onclick={erase} label='CE' />
+                        <Button hl onclick={erase} label='DEL' />
+                        <Button clear onclick={clear} label='C' />
                     </div>
 
                     <div className={styles['digit-buttons']}>
@@ -83,7 +113,7 @@ export const Calculator = () => {
                     <div className={styles['side-buttons']}>
                         <Button hl onclick={addLabel} label='+' />
                         <Button hl onclick={addLabel} label='-' />
-                        <Button isSubmit onclick={handleSubmit} label='=' />
+                        <Button canSubmit={canSubmit} isSubmit onclick={handleSubmit} label='=' />
                     </div>
 
                 </div>
