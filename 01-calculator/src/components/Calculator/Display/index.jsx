b3a3215d85efc7d46dc0e5bb88c82bc884d30a67
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-10-30 12:39:00 -0300

    refactor: remove unused lines

--- 01-calculator/src/components/Calculator/Display/index.jsx
+++ 01-calculator/src/components/Calculator/Display/index.jsx
@@ -1,12 +1,6 @@
 import styles from './display.module.css';
 
-import { useEffect } from 'react';
-
 export const Display = ({ result }) => {
-    useEffect(() => {
-
-    }, [result])
-
     return (
         <div className={styles.container}>
             <span className={styles.result}>{result}</span>
